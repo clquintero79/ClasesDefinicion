51a8f532a90a36d149d7b7970a0dc325fc4ac758
unknown <Sala300@TUNDFPCGE10F310.FormacionSena.RED>
2019-08-28 12:58:05 -0500

    Initial commit

---
+++ ClasesDefinicio/ClasesDefinicio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClasesDefinicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
